32552553d8abba4cb8b103d97d5e7e633110169c
Mayckon Rebecci <mayckon_mmr@hotmail.com>
2021-08-14 02:15:00 -0300

    first commit of this app

---
+++ script.js
@@ -0,0 +1,44 @@
+
+let divBtn = document.querySelector("#div-btn");
+
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255 + 1);
+    const g = Math.floor(Math.random() * 255 + 1);
+    const b = Math.floor(Math.random() * 255 + 1);
+
+    return `rgb( ${r}, ${g}, ${b} )`;
+};
+
+//-----------------------------------------------------
+//* This is the 1st Way
+//-----------------------------------------------------
+const createBtn = () => {
+    let newBtn = document.createElement("button");
+    divBtn.appendChild(newBtn);
+    newBtn.innerText = "CLICK";
+    newBtn.setAttribute("id", "btn");   
+
+    newBtn.addEventListener("click", function () {        
+        this.style.backgroundColor = randomColor();        
+    })   
+}
+
+for (let i = 0; i <= 99; i++) {
+    createBtn();
+}
+
+
+//-----------------------------------------------------
+//* This is the 2nd Way
+//-----------------------------------------------------
+// for (let i = 0; i <= 99; i++) {
+//     createBtn();
+// }
+
+// let newBtns = document.querySelectorAll("#btn");
+
+// for (let newBtn of newBtns){
+//     newBtn.addEventListener("click", function () {
+//         newBtn.style.backgroundColor = randomColor();
+//     })
+// }
